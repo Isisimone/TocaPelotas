4d823f837c55f5ef5b5db4498392f8aac0597a4c
Isis Torres <isis_simone@hotmail.com>
2026-06-16 11:53:12 +0200

    Delete Toca Pelotas directory

--- Toca Pelotas/Scripts/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace TocaPelotas
-{
-    public static class Constants
-    {
-        public const float VERTICAL_FACTOR = 1.2f;
-        public const float HORIZONTAL_FACTOR = 0.8f;
-    }
-}
